5bd6e484866bf080c929041c6874716dfc59f600
Shahnawaz Sazid <shahnawazsazid60@gmail.com>
2025-06-19 22:33:28 +0600

    Book Model Added

---
+++ src/app/models/book.model.ts
@@ -0,0 +1,32 @@
+import { model, Schema } from "mongoose";
+
+const bookSchema = new Schema({
+  title: { type: String, required: [true, "Title Is Required"] },
+  author: { type: String, required: [true, "Author Is Required"] },
+
+  genre: {
+    type: String,
+    required: [true, "Genre Is Required"],
+    enum: {
+      values: [
+        "FICTION",
+        "NON_FICTION",
+        "SCIENCE",
+        "HISTORY",
+        "BIOGRAPHY",
+        "FANTASY",
+      ],
+      message: "{VALUE} Is Not A Valid Genre",
+    },
+  },
+  isbn: { type: String, required: [true, "isbn Is Required"], unique: true },
+  description: { type: String },
+  copies: {
+    type: Number,
+    required: [true, "Copies Area Required"],
+    min: [0, "Copies Must be a Non Negative Number"],
+  },
+  available: { type: Boolean, default: true },
+});
+
+export const Book = model("Book", bookSchema);
